3e5c1350365efa522bd57d8d57536de1a31141a7
durgaprasad226 <durgaprasadmalthumkar@gmail.com>
2023-11-24 17:48:27 +0530

    Initial Commit

---
+++ Collections/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] arr=new int[10];
+            Array.Resize(ref arr, 15);*/  //Array
+
+            ArrayList al = new ArrayList(); //Creating a new ArrayList
+            Console.WriteLine(al.Capacity);
+            al.Add(12);
+            Console.WriteLine(al.Capacity); //Finding capacity of ArrayList
+            al.Add(26);
+            al.Add(32);                     //Adding the values into AL
+            al.Add(11);
+            al.Add(14);
+            Console.WriteLine(al.Capacity);
+            foreach (object obj in al)
+            {
+                Console.Write(obj + " ");
+                Console.WriteLine();
+            }
+            al.Insert(2, 25);              //Inserting based on the index value
+            foreach (object obj in al)
+            {
+                Console.Write(obj + " ");
+                Console.WriteLine();
+            }
+            al.Remove(25);                 //Removing the value directly
+            al.RemoveAt(4);                //Removing using Index position
+            foreach (object obj in al)
+                Console.Write(obj + " ");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht=new Hashtable();
+            ht.Add("EmpId", 1267);
+            ht.Add("Ename", "Durga Prasad");
+            ht.Add("Dept", "MCB");
+            ht.Add("Role", "Dotnet Developer");
+            ht.Add("Email", "dp@gmail.com");
+            ht.Add("Phone", 6304195695);
+            ht.Add("Company", "Carbynetech");
+            ht.Add("Location", "Hi-Tech City");
+            ht.Add("Salary", 18000);
+            Console.WriteLine(ht.Count);
+            Console.WriteLine(ht.ContainsValue("Durga Prasad"));
+            Console.WriteLine(ht.ContainsValue("Hello"));     //Checks whether given value is present or not
+            Console.WriteLine(ht.ContainsKey("EmpId"));
+            Console.WriteLine(ht.ContainsKey("StdId"));       //Checks whether given key is present or not
+            ht.Remove("Dept");                                //Removes the value with given key
+            Console.WriteLine("DP".GetHashCode());
+            Console.WriteLine("=========Only Keys=========");
+            foreach (object key  in ht.Keys)
+            {
+                Console.WriteLine(key);
+            }
+            Console.WriteLine("=========Only Values=========");
+            foreach (object value in ht.Values)
+            {
+                Console.WriteLine(value);
+                //Console.WriteLine(ht[key]); value or ht[key] are similar
+            }
+            Console.WriteLine("=========Key : Value pair=========");
+            foreach (object key in ht.Keys)
+            {
+                Console.WriteLine(key + " : " + ht[key]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
